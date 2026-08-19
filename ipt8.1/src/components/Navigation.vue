f8b28821622107fe256775092b6466ddbbf0198a
zoe bachmann <zoe_bachmann@sluz.ch>
2026-08-19 12:51:26 +0200

    fixed #4 abb nav

---
+++ ipt8.1/src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<script lang="js">
+import router from '@/router';
+import {route} from 'vue-router'
+</script>
+<template>
+  <div class="navigation">
+    <router-link to="/">Home</router-link>
+    <router-link to="/zoe">Zoe</router-link>
+    <router-link to="/gian">Gina</router-link>
+    <router-link to="/sanjivan">Sanjivan</router-link>
+    <router-link to="/gian">Gian</router-link>
+  </div>
+</template>
